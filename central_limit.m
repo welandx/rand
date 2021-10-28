5aa66996a1a740adc3c3bb8d6700529c43c9a724
welandx <weland@bupt.edu.cn>
2021-10-28 23:14:18 +0800

    box-muller

--- central_limit.m
+++ central_limit.m
@@ -1,3 +1,8 @@
-a=textscan('centarl_limit.txt','%f');
+a=textread('central_limit.txt');
+
+x=linspace(-4,4);
+y=pdf('Normal',x,-0.0343,1.0091);
+plot(x,y,'LineWidth',2)
+
 mean(a)
-var(a)
+sqrt(var(a))
